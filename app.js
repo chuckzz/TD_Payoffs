3746059f10a7548192e3912e4bcce4f4d5fa5e81
Ray Hou <tlhou@uwaterloo.ca>
2014-09-09 14:20:24 -0400

    TD_Week 2

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express')
+  , mongoskin = require('mongoskin')
+  , bodyParser = require('body-parser')
+  routes = require('./routes'),
+  api = require('./routes/api')
+
+var app = express()
+app.use(bodyParser())
+var path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+
+var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true})
+
+app.set('view engine', 'jade')
+
+app.param('collectionName', function(req, res, next, collectionName){
+  req.collection = db.collection(collectionName)
+  return next()
+})
+
+//app.get('/', function (req, res) {
+  //res.render('index', { title: 'Hey', message: 'Hello there!'});
+//})
+
+app.get('/collections/:collectionName', function(req, res, next) {
+  req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+app.post('/collections/:collectionName', function(req, res, next) {
+  req.collection.insert(req.body, {}, function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+app.get('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.findById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send(result)
+  })
+})
+
+app.put('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
+    if (e) return next(e)
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+})
+
+app.del('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.removeById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+})
+
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+app.listen(3000)
